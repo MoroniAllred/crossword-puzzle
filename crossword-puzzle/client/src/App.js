5da4b0872bef00a4420cfb4f518b7e7b8c293ba4
Moroni Allred <moroniallred@gmail.com>
2019-07-21 16:20:32 -0600

    commit

--- crossword-puzzle/client/src/App.js
+++ crossword-puzzle/client/src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import './App.css';
 
 class App extends Component {
-    constructor(props) {
+    construcchetor(props) {
         super(props);
         this.state = { 
             word: ""
