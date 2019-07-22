f2bd5380f4c4813c8a6c73d495fc10a57db2bd24
Moroni Allred <moroniallred@gmail.com>
2019-07-21 21:33:16 -0600

    word chooser dosn't work yet

--- crossword-puzzle/client/src/App.js
+++ crossword-puzzle/client/src/App.js
@@ -1,45 +1,13 @@
 import React, { Component } from 'react';
-import axios from "axios";
 import './App.css';
-// import WordGetter from "./components/WordGetter.js"
+import WordGetter from "./components/WordGetter.js"
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            word: []
-        }
-    }
-
-    getWord = () => {
-        axios.get("/word")
-        .then(response => {
-            const wordArr = []
-            
-            for(let i = 0; i < response.data.length; i++){
-                wordArr.push(response.data[i].word)
-                // console.log(wordArr)
-                this.setState({word: wordArr})
-                // console.log(response.data[i].word)
-            }
-            console.log(this.state.word)
-        })
-        
-    }
-    
-    
-    render() { 
-        const allwords = this.state.word.map(words => <h1>{words}</h1>)
-        
-        return ( 
-            <div className="word" onClick={this.getWord}>
-                {/* <h1 className="word" onClick={this.getWord}> */}
-                    {allwords}
-                    {/* {this.state.word} */}
-                {/* </h1> */}
-            </div>
-         );
-    }
+const App = () =>{
+    return ( 
+        <div>
+            <WordGetter/>
+        </div>
+    );
 }
  
 export default App;
--- crossword-puzzle/client/src/components/WordGetter.js
+++ crossword-puzzle/client/src/components/WordGetter.js
@@ -1,12 +1,48 @@
-import React from "react"
+import React, {Component} from "react"
+import axios from "axios"
 
-const WordGetter = (props) => {
-    
-    return(
-        <div>
-            <h1>{props.word}</h1>
-        </div>
-    )
+class WordGetter extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { 
+            word: []
+        }
+    }
+
+    getWord = () => {
+        axios.get("/word")
+        .then(response => {
+            const wordArr = []
+            
+            for(let i = 0; i < response.data.length; i++){
+                wordArr.push(response.data[i].word)
+                // console.log(wordArr)
+                this.setState({word: wordArr})
+                // console.log(response.data[i].word)
+            }
+        })
+        
+    }
+
+    // wordChooser = () => {
+    //     for(let i = 0; i> 10; i++){
+    //         const used = []
+    //         const choosen = Math.floor(Math.random() * 61)
+    //         console.log(choosen)
+    //     }
+    // }
+
+    render(){
+        const mappedWords=this.state.word.map(words => <h1>{words}</h1>)
+
+        return(
+            <div>
+                <div className="word" onClick={this.getWord}>
+                    {mappedWords}
+                </div>
+                <button onClick={this.wordChooser}></button>
+            </div>
+        )
+    }
 }
-
 export default WordGetter
